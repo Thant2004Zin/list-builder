5c8c0a805dbae5ee1a743a976b2654f4c5623e41
Thant2004Zin` <frequency307@gmail.com>
2022-06-16 13:36:52 +0630

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+        <h1>{{ title }}</h1>
+        <ul>
+            <li v-for="(list,index) in lists" v-bind:key="index">
+                {{ list }}
+                <button v-on:click="Del(index)">Del</button>
+            </li>
+        </ul>
+
+
+        <form action="" v-on:submit.prevent="addList">
+            <input type="text" v-model="input" required>
+            <button>Add</button>
+        </form>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+      data() {
+        return {
+          title: "List Builder",
+          lists :  [
+              "To eat",
+              "To sleep",
+          ]
+        }
+      },
+        methods:{
+          addList(){
+              this.lists.push(this.input)
+              this.input = null
+          },
+          Del(index){
+              this.lists.splice(index,1)
+          }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+    @import "boo";
+
 </style>
